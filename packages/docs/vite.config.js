61a80d3d28f9f1c49c0d662222b79b4facb785b3
Arley Souto <macx15@hotmail.com>
2023-12-15 19:15:37 -0300

    chore: ajustado storybook

---
+++ packages/docs/vite.config.js
@@ -0,0 +1,9 @@
+/* é para o ts entender que estamos usando o vite e com isso para usar algumas dependencias */
+/// <reference types="vite/client"/> 
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins:[react()]
+})
