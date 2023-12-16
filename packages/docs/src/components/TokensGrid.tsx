593c5458b5c5cabfa1a380cdefd06ab65b528a84
Arley Souto <macx15@hotmail.com>
2023-12-15 21:43:32 -0300

    feat: criado o spaces

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,34 @@
+import '../styles/tokens-grid.css'
+
+
+interface TokensGridProps {
+    tokens: Record<string, string>
+    hasremValue?: boolean
+}
+
+export function TokensGrid({tokens, hasremValue = false}:TokensGridProps){
+    return(
+        <table className='tokens-grid'>
+            <thead>
+                <th>Name</th>
+                <th>Value</th>
+                {hasremValue && <th>Pixels</th>}
+            </thead>
+            <tbody>
+                {
+                  Object.entries(tokens).map(([key, value]) =>{
+                    return(
+                        <tr key={key}>
+                           <td>{key}</td> 
+                           <td>{value}</td> 
+                          {hasremValue && (
+                          <td>{Number(value.replace('rem', '')) * 16}px</td>
+                          ) }
+                        </tr>
+                    )
+                  })  
+                }
+            </tbody>
+        </table>
+    )
+}
