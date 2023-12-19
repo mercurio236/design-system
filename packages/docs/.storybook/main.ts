ff7e7463ba9944ad0cae5419bbcef0e2b7ce7061
Arley Souto <macx15@hotmail.com>
2023-12-18 23:28:44 -0300

    ci: configurado main

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -29,7 +29,7 @@ const config: StorybookConfig = {
   },
   
 
-  viteFinal: async (config, { configType }) => {
+  async viteFinal(config, { configType }){
     if (configType === 'PRODUCTION') {
       config.base = '/design-system/'
     }
