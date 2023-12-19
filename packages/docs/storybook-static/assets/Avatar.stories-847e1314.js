6fb0b433db8c19b02c34fe06ad90055dfc6de74e
Arley Souto <macx15@hotmail.com>
2023-12-19 00:35:52 -0300

    ci: teste de funcionalidade

---
+++ packages/docs/storybook-static/assets/Avatar.stories-847e1314.js
@@ -0,0 +1,5 @@
+import{A as p}from"./index-4cdfc543.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./index-d3ea75b5.js";import"./jsx-runtime-ffb262ed.js";const y={title:"Data display/Avatar",component:p,args:{src:"https://github.com/mercurio236.png",alt:"Arley Souto"},argTypes:{children:{control:{type:"text"}}}},r={},a={args:{src:"undefined"}};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};var s,c,i;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    src: 'undefined'
+  }
+}`,...(i=(c=a.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const h=["Primary","WithFallback"];export{r as Primary,a as WithFallback,h as __namedExportsOrder,y as default};
---
+++ packages/docs/storybook-static/assets/MultiStep.stories-003a51e0.js
@@ -0,0 +1,7 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{M as i,B as u}from"./index-4cdfc543.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./index-d3ea75b5.js";const f={title:"Form/Multi Step",component:i,args:{size:4,currentStep:1},decorators:[m=>n.jsx(u,{children:m()})]},r={args:{}},e={args:{currentStep:4}};var t,s,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var a,p,c;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    currentStep: 4
+  }
+}`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};const j=["Primary","Full"];export{e as Full,r as Primary,j as __namedExportsOrder,f as default};
